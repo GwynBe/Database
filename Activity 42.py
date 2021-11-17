6910c9135e5b00f176597d4010eba72188b6220e
GwynBe <90431456+GwynBe@users.noreply.github.com>
2021-11-17 03:02:06 -0800

    Update Activity 42.py

--- Activity 42.py
+++ Activity 42.py
@@ -8,22 +8,10 @@ database="store_cms_plusplus"
 
 mycursor = mydb.cursor()
 
-class Counter:
-    def __init__(self, maker, quantity):
-        self.maker = maker
-        self.quantity = quantity
-    def __str__(self):
-        return "Maker: %s, Quantity: %s" % (self.maker, self.quantity)
-
 def getCounterbyMaker():
     mycursor.execute(f"SELECT maker, COUNT(maker) as 'quantity' FROM store_cms_plusplus.laptop GROUP BY maker ORDER BY quantity DESC;")
     result = mycursor.fetchall()
-    result_list = []
-    for x in result:
-        counter = Counter(x[0], x[1])
-        result_list.append(counter)
-    print(result_list)
-    for x in result_list:
-        print(x)
+    return result
 
-getCounterbyMaker()
+for i in getCounterbyMaker():
+    print(f"Maker: {i[0]}, Quantity: {i[1]}")
